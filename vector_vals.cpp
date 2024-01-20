56b9fec27243ae9eebd588cd888becb198bada4e
DawidS11 <dawidsmalcuga@gmail.com>
2024-01-20 18:35:40 +0100

    Fix data races with mutex and atomic

--- vector_vals.cpp
+++ vector_vals.cpp
@@ -2,36 +2,41 @@
 #include <thread>
 #include <vector>
 #include <iomanip>
+#include <mutex>
+#include <atomic>
 
-// g++ -std=c++20 vector_vals.cpp -o prog
+// g++ -fsanitize=thread -std=c++20 vector_vals.cpp -o prog
 bool isRunning = true;
+time_t start = time(0);
+std::mutex myLock;
+std::atomic<time_t> timeLeft = 10;
 
 void change_vals(std::vector<int>& vec, int index) {
-    while(isRunning) {
+    while(timeLeft > 0) {
         std::this_thread::sleep_for(std::chrono::milliseconds(200*(index+1)));
-        vec[index] = rand() % 100;
+        myLock.lock();
+            vec[index] = rand() % 100;
+        myLock.unlock();
     }
 }
 
 int main() {
-    srand(2024);
+    srand(time(0));
     std::vector<int> vals(4);
     std::vector<std::thread> threads;
-    time_t start, timeLeft;
 
     for(int i = 0; i < 4; ++i) {
         threads.push_back(std::thread(change_vals, std::ref(vals), i));
     }
 
-    start = time(0);
-    while(isRunning) {
+    while(timeLeft > 0) {
         std::this_thread::sleep_for(std::chrono::milliseconds(300));
-        //system("clear");
         for(int i = 0; i < 4; ++i) {
-            std::cout << std::setw(5) << vals[i] << std::setw(5);
+            myLock.lock();
+                std::cout << std::setw(5) << vals[i] << std::setw(5);
+            myLock.unlock();
         }
-        timeLeft = 10 - (time(0) - start);
-        isRunning = timeLeft > 0;
+        timeLeft -= (time(0) - start);
         std::cout << "   | time left: " << timeLeft << std::endl;
     }
     for(auto& t : threads) {
