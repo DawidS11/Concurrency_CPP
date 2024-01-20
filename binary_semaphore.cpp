119207d662e47bacfaab9a33143d16643effd1b3
DawidS11 <dawidsmalcuga@gmail.com>
2024-01-20 19:14:02 +0100

    Add another semaphore and change to binary_semaphore

--- binary_semaphore.cpp
+++ binary_semaphore.cpp
@@ -2,45 +2,41 @@
 #include <thread>
 #include <chrono>
 #include <semaphore>
-#include <mutex>
 #include <assert.h>
 
 /*
 Two threads adding to the result and three threads subtracting from it.
-Semaphore is set two 1 so both subtraction and addition may occur first.
-Mutexes are used in order not to subtract/add at the same time.
+Semaphore of subtraction is set two 1 so the subtraction will occur first.
+Subtraction and addition should occur alternately.
 */
 
-// g++ -std=c++20 counting_semaphore.cpp -o prog
+// g++ -fsanitize=thread -std=c++20 binary_semaphore.cpp -o prog
 
-std::counting_semaphore gSemaphore{1};
+std::binary_semaphore addSemaphore{0};
+std::binary_semaphore subtractSemaphore{1};
 
 int main(){
 
     int result = 2;
 
-    std::mutex subtractLock;
     auto subtract = [&](int i){
-        gSemaphore.acquire();
-        subtractLock.lock();
+        subtractSemaphore.acquire();
             std::this_thread::sleep_for(std::chrono::seconds(1));
             result -= 2;
             std::cout << "Subtract " << i << ", result is: " << result << std::endl;
-        subtractLock.unlock();
+        addSemaphore.release();
     };
 
     std::thread subtract1(subtract, 1);
     std::thread subtract2(subtract, 2);
     std::thread subtract3(subtract, 3);
 
-    std::mutex addLock;
     auto add = [&](int i) {
-        addLock.lock();
-            std::this_thread::sleep_for(std::chrono::seconds(2));
+        addSemaphore.acquire();
+            std::this_thread::sleep_for(std::chrono::seconds(1));
             result += 2;
             std::cout << "Add " << i << ", result is: " << result << std::endl;
-        addLock.unlock();
-        gSemaphore.release();
+        subtractSemaphore.release();
     };
 
     std::thread add1(add, 1);
